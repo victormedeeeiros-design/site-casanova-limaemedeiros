24438e37430d6cc47c986992d624c066390ef945
victormedeeeiros-design <victormedeeeiros@gmail.com>
2025-09-28 00:14:52 +0000

    correcaobugpaineladm

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,7 @@ const App = () => (
       <Routes>
         <Route path="/" element={<Index />} />
         <Route path="/admin/login" element={<AdminLogin />} />
+        <Route path="/admin" element={<AdminPanel />} />
         <Route path="/admin/dashboard" element={<AdminDashboard />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
